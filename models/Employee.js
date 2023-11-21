31a5edb43e9fc3ff9fba4a65c5f7dad04b901783
21020764-VuPhuongHong <21020764@vnu.edu.vn>
2023-11-21 17:28:53 +0700

    update branch

--- models/Employee.js
+++ models/Employee.js
@@ -10,16 +10,13 @@ const Employee = new Schema({
     firstName: { type: String, default: '' },
     lastName: { type: String, default: '' },
     birth: {type: Date},
-    role: {type: String, enum: ['StationE', 'StationAd', 'WarehouseAd', "WarehouseE", "WarehouseE", 'Manager'], default: 
+    role: {type: String, enum: ['StationE', 'StationAd', 'WarehouseAd', "WarehouseE", 'Manager'], default: 
 'StationE'},
     avatar: { type: String, default: 'https://icones.pro/wp-content/uploads/2021/11/icone-vert-de-camion-d-expedition-et-de-livraison.png' },
     email: { type: String, default: '' },
     phoneNumber: { type: String, default: '' },
     address: {type: String},
     workAddress: {type: String},
-    workPlaceId: {type: String},
-    resetToken: {type: String, default: ''},
-    resetTokenExpiration: {type: Date, default: 0},
     resetToken: {type: String, default: ''},
     resetTokenExpiration: {type: Date, default: 0},
     createdAt: { type: Date },
