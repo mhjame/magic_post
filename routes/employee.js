bfcba7865fde10cb21b4e65652714010e784f349
21020764-VuPhuongHong <21020764@vnu.edu.vn>
2023-12-01 23:23:03 +0700

    add create order for shipping to des station

--- routes/employee.js
+++ routes/employee.js
@@ -5,6 +5,8 @@ const employeeController = require('../controllers/EmployeeController')
 
 router.get('/create_to_wh_order', employeeController.createShipToWarehouseOrder); 
 router.post('/stored/to_wh_order', employeeController.postShipToWarehouseOrder); 
+router.get('/create_to_station_order', employeeController.createShipToStationOrder);
+router.post('/stored/to_station_order', employeeController.postShipToStationOrder); 
 
 
 module.exports = router;
