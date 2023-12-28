7b116263ee9d4eb1885d4b35b6f5f58a73b4e7ff
Nguyễn Ngọc Linh <100554756+nngoclinh1203@users.noreply.github.com>
2023-12-28 23:24:06 +0700

    add commit

--- routes/statistic.js
+++ routes/statistic.js
@@ -6,8 +6,7 @@ const passwordController = require('../controllers/PasswordController');
 const statisticController = require('../controllers/StatisticController');
 
 
-// router.get('/statistic', statisticController.getPostStatisticsStation)
-//
+//-----------------MANAGER--------------------------------
 router.get('/statistic_manager', statisticController.postStatisticManagerDailyNationWide)
 router.post('/statistic_manager/daily', statisticController.postStatisticManagerDailyNationWide)
 router.post('/statistic_manager/week', statisticController.postStatisticManagerWeekNationWide)
@@ -27,19 +26,19 @@ router.get('/statistic_manager_warehouse/daily/:warehouseCode', statisticControl
 router.get('/statistic_manager_warehouse/week/:warehouseCode', statisticController.getStatisticManagerWeekWarehouse)
 router.get('/statistic_manager_warehouse/month/:warehouseCode', statisticController.getStatisticManagerMonthWarehouse)
 router.get('/statistic_manager_warehouse/year/:warehouseCode', statisticController.getStatisticManagerYearWarehouse)
-//-------
+//-----------------STATION AD--------------------------------
 router.get('/statistic_stationAd/:stationCode', statisticController.getStatisticDailyStation)
 router.get('/statistic_stationAd/daily/:stationCode', statisticController.getStatisticDailyStation)
 router.get('/statistic_stationAd/week/:stationCode',  statisticController.getStatisticWeekStation)
 router.get('/statistic_stationAd/month/:stationCode', statisticController.getStatisticMonthStation)
 router.get('/statistic_stationAd/year/:stationCode', statisticController.getStatisticYearStation)
-//-------
+//-----------------WAREHOUSE AD--------------------------------
 router.get('/statistic_warehouseAd/:warehouseCode', statisticController.getStatisticDailyWarehouse)
 router.get('/statistic_warehouseAd/daily/:warehouseCode', statisticController.getStatisticDailyWarehouse)
 router.get('/statistic_warehouseAd/week/:warehouseCode', statisticController.getStatisticWeekWarehouse)
 router.get('/statistic_warehouseAd/month/:warehouseCode', statisticController.getStatisticMonthWarehouse)
 router.get('/statistic_warehouseAd/year/:warehouseCode', statisticController.getStatisticYearWarehouse)
-//---------
+//-----------------STATION EMPLOYEE--------------------------------
 router.get('/statistic_stationE/:employeeId', statisticController.getStatisticStationEmployeeDaily)
 router.get('/statistic_stationE/daily/:employeeId', statisticController.getStatisticStationEmployeeDaily)
 router.get('/statistic_stationE/week/:employeeId', statisticController.getStatisticStationEmployeeWeek)
